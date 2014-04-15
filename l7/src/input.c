4dc41e0edaec585e097e79b258e88c1f6bcf7449
ashon <ashon8813@gmail.com>
2014-04-16 00:57:25 +0900

    update

--- l7/src/input.c
+++ l7/src/input.c
@@ -13,6 +13,7 @@
 #include <string.h>
 
 #define MAX_BUF 255
+#define MAX_LINE 100
 
 // var type ULL - unsigned long long
 #define ULL unsigned long long
@@ -87,4 +88,57 @@ ULL read_number()
 
     return number;
 }
+
+/*
+ * read lines.
+ */
+ULL read_lines(ULL mode)
+{
+    ULL lines;
+    printf("input lines..(1 to %d%s > ",
+        MAX_LINE, mode == 3 ? ", odd numbers only..)" : ")");
+    lines = read_number();
+
+    if(mode == 3)
+        if((lines & 0x1) != 1)
+            return 0;
+
+    if(0 < lines && lines < MAX_LINE + 1)
+        return lines;
+}
+
+/**
+ * read mode.
+ */
+ULL read_mode()
+{
+    printf("\n# Draw Pattern.\n");
+    printf(" 1 - lower triangle\n");
+    printf(" 2 - upper triangle\n");
+    printf(" 3 - diamond\n");
+    printf(" 4 - parallelogram\n");
+    printf(" 5 - upper obtuse triangle\n");
+    printf("select mode > ");
+
+    switch(read_number()) {
+        case 1:
+            printf("\n# draw lower triangle\n");
+            return 1;
+        case 2:
+            printf("\n# draw upper triangle\n");
+            return 2;
+        case 3:
+            printf("\n# draw diamond triangle\n");
+            return 3;
+        case 4:
+            printf("\n# draw parallelogram\n");
+            return 4;
+        case 5:
+            printf("\n# draw upper obtuse triangle\n");
+            return 5;
+    }
+    printf("# invalid mode number..\n");
+    return 0;
+}
+
 
--- l7/src/run.c
+++ l7/src/run.c
@@ -7,72 +7,228 @@
  * @date 2014-04-15
  *
  */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define MAX_LINE 100
 #define ULL unsigned long long
-/**
- * read mode.
- */
-ULL read_mode()
-{
-    printf("\n# Draw Pattern.\n");
-    printf(" 1 - lower triangle\n");
-    printf(" 2 - upper triangle\n");
-    printf(" 3 - diamond\n");
-    printf(" 4 - parallelogram\n");
-    printf(" 5 - upper obtuse triangle\n");
-    printf("select mode > ");
 
-    switch(read_number()) {
-        case 1:
-            printf("\n# draw lower triangle\n");
-            return 1;
-        case 2:
-            printf("\n# draw upper triangle\n");
-            return 2;
-        case 3:
-            printf("\n# draw diamond triangle\n");
-            return 3;
-        case 4:
-            printf("\n# draw parallelogram\n");
-            return 4;
-        case 5:
-            printf("\n# draw upper obtuse triangle\n");
-            return 5;
-    }
-    printf("# invalid mode number..\n");
+/**
+ * single list of draw_cmd.
+ */
+typedef struct cmd_node_ {
+    void(*cmd)(ULL, ULL, ULL);
+    struct cmd_node_* next;
+} cmd_node;
+
+/**
+ * add
+ */
+cmd_node* add_cmd_node(cmd_node* n, void(*cmd)(ULL, ULL, ULL))
+{
+    cmd_node* target = (cmd_node*)malloc(sizeof(cmd_node));
+    target->cmd = cmd;
+
+    if(n)
+        n->next = target;
+    target->next = 0;
+    return target;
+}
+
+/**
+ * getter
+ */
+void* get_cmd(cmd_node* n)
+{
+    return n->cmd;
+}
+
+/*
+ * free node
+ *
+ * init. p = head
+ *    head -> [ ]-> [ ]-> [ ]-> ... -> 0
+ *             ^ - *p
+ * case 0: sizeof head == 0
+ *    return 0;
+ *
+ * case 1: size of head == 1
+ *  i. head -> [ ]
+ *             ^ - *p
+ *  1. free(p)
+ *    head -> [x]
+ *             ^ - *p
+ *  
+ * case 2: size of head > 1
+ *  i. head -> [ ]-> [ ]-> [ ]-> ... -> 0
+ *             ^ - *p
+ * ## do loop
+ *  1. q = p
+ *    head -> [ ]-> [ ]-> [ ]-> ... -> 0
+ *             ^ - *p, *q
+ *
+ *  2. p = p->next
+ *    head -> [ ]-> [ ]-> [ ]-> ... -> 0
+ *             =     ^ - *p
+ *             ^ - *q
+ *  3. free(q)
+ *    head -> [x]-> [ ]-> [ ]-> ... -> 0
+ *             =     ^ - *p
+ *             ^ - *q
+ *
+ * ## while ( p != null )
+ *    head -> [x]-> [x]-> [x]-> ... [x]-> 0
+ *             =     =     =    =..  =    ^ - *p
+ *             =     =     =    =..  ^ - *q
+ *  
+ * @param
+ *    cmd_node* - head of draw command list
+ *
+ * @return
+ *    0 - 
+ */
+int free_node(cmd_node* head)
+{
+    cmd_node* p = head;
+    cmd_node* q;
+    do {
+        q = p;
+        p = p->next;
+        free(q);
+    } while(p);
     return 0;
 }
 
 /*
- * read lines.
+ * draw strategies..
  */
-ULL read_lines(ULL mode)
+void draw_loop_up(ULL lines, cmd_node* head)
 {
-    ULL lines;
-    printf("input lines..(1 to %d%s > ",
-        MAX_LINE, mode == 3 ? ", odd numbers only..)" : ")");
-    lines = read_number();
-
-    if(mode == 3)
-        if((lines & 0x1) != 1)
-            return 0;
-
-    if(0 < lines && lines < MAX_LINE + 1)
-        return lines;
-}
-
-int main() {
-    ULL mode;
-    ULL lines;
-
-    while(1){
-        mode = read_mode();
-        if(mode){
-            lines = read_lines(mode);
-            draw(mode, lines);
-        }
+    ULL i, j;
+    cmd_node* cur;
+    for(i = 1; i < lines + 1; i++) {
+        for(cur = head; cur; cur = cur->next)
+            if(cur->cmd)
+                cur->cmd(lines, i, j);
+        printf("\n");
     }
-    return 0;
 }
+
+void draw_loop_down(ULL lines, cmd_node* head)
+{
+    ULL i, j;
+    cmd_node* cur;
+    for(i = lines - 1; 0 < i; i--) {
+        for(cur = head; cur; cur = cur->next)
+            if(cur->cmd)
+                cur->cmd(lines, i, j);
+        printf("\n");
+    }
+}
+
+/*
+ * draw pattern Commands
+ *
+ * command function type
+ *  void(*)(ULL, ULL, ULL)
+ */
+void draw_dia_gap(ULL lines, ULL i, ULL j)
+{
+    for(j = lines - i; 0 < j; j--)
+        printf(" ");
+}
+
+void draw_dia_part(ULL lines, ULL i, ULL j)
+{
+    for(j = 0; j < (i << 1) - 1; j++)
+        printf("*");
+}
+
+void draw_lower_part(ULL lines, ULL i, ULL j)
+{
+    for(j = 0; j < i; j++)
+        printf("*");
+}
+
+void draw_upper_gap(ULL lines, ULL i, ULL j)
+{
+    for(j = lines - i; 0 < j; j--)
+        printf(" ");
+}
+
+void draw_upper_part(ULL lines, ULL i, ULL j)
+{
+    for(j = lines + 1 - i; 0 < j; j--)
+        printf("*");
+}
+
+void draw_parallelo_part(ULL lines, ULL i, ULL j)
+{
+    for(i = 0; i < lines; i++)
+        printf("*");
+}
+
+/*
+ * draw shapes.
+ */
+int draw(ULL mode, ULL lines)
+{
+    ULL i, j;
+
+    cmd_node* head;
+    cmd_node* cur;
+
+    void(*loop)(ULL, cmd_node*) = 0;
+
+    if(mode == 0)
+        return 1;
+
+    if(lines) {
+        head = add_cmd_node(0, 0);
+        cur = head;
+        switch(mode){
+            case 1:
+                cur = add_cmd_node(cur, draw_lower_part);
+                loop = draw_loop_up;
+            break;
+            case 2:
+                cur = add_cmd_node(cur, draw_upper_part);
+                loop = draw_loop_up;
+            break;
+            case 3:
+                lines = (lines + 1) >> 1;
+                cur = add_cmd_node(cur, draw_dia_gap);
+                cur = add_cmd_node(cur, draw_dia_part);
+                draw_loop_up(lines, head);
+                free_node(head);
+
+                head = add_cmd_node(0, 0);
+                cur = head;
+                cur = add_cmd_node(cur, draw_dia_gap);
+                cur = add_cmd_node(cur, draw_dia_part);
+                loop = draw_loop_down;
+            break;
+            case 4:
+                cur = add_cmd_node(cur, draw_upper_gap);
+                cur = add_cmd_node(cur, draw_parallelo_part);
+                loop = draw_loop_up;
+            break;
+            case 5:
+                cur = add_cmd_node(cur, draw_upper_gap);
+                cur = add_cmd_node(cur, draw_upper_part);
+                loop = draw_loop_up;
+            break;
+        } // end switch
+
+        // draw
+        if(loop)
+            loop(lines, head);
+
+        // free
+        free_node(head);
+    } else
+        printf("# invalid line number..\n");
+    return 1;
+}
+
